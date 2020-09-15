7afd138f762470de79a668426d2802c040956f70
Chris Brooks-Prenger <brooksprench@hdsb.ca>
2020-09-15 09:46:54 -0400

    Initial commit

---
+++ Example pgZero/newFile.py
@@ -0,0 +1,14 @@
+#-----------------------------------------------------------------------------
+# Name:        New File Generator (newFile.py)
+# Purpose:     Generates a new file for use in the ICS3U course
+#
+# Author:      Mr. Brooks
+# Created:     13-Sept-2020
+# Updated:     13-Sept-2020
+#-----------------------------------------------------------------------------
+
+WIDTH = 300
+HEIGHT = 300
+
+def draw():
+    screen.fill((128, 0, 0))
---
+++ README.md
@@ -0,0 +1,2 @@
+# Blank-Repo-Template
+This is where you will keep your homework and other assignments for the class that are not necessarily going to be graded.
